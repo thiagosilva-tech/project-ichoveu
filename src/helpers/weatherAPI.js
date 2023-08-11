772d8b19ed6362f709cce46003e8e4469bcd50b7
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-08-11 11:12:17 -0300

    feat: requisito 01

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,14 @@
 // Remova os comentários a medida que for implementando as funções
-
-export const searchCities = (/* term */) => {
-//   seu código aqui
+const token = import.meta.env.VITE_TOKEN;
+const API_BASE = 'http://api.weatherapi.com/v1/search.json?lang=pt&key=';
+export const searchCities = async (term) => {
+  //   seu código aqui
+  const res = await fetch(`${API_BASE}${token}&q=${term}`);
+  const data = await res.json();
+  if (data.length > 0) {
+    return data;
+  }
+  return window.alert('Nenhuma cidade encontrada');
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
